aafd41f28dda785580524d55bdaeb66422c8c75c
andresborn <andres.born@gmail.com>
2021-02-01 11:12:58 -0600

    Created getCard and getArrayofCards functions

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css";
 
-const cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-const suits = ["♦", "♥", "♣", "♠"]
-
 const cardsContainer = document.querySelector("#cards-container")
 
-const getRandom = (array) => {
-    const rand = Math.floor((Math.random() * array.length))
-    return array[rand]
+const getRandomCard  = (() => {
+    const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    const suits = ["♦", "♥", "♣", "♠"]
+
+    const getRandom = (array) => {
+        const rand = Math.floor((Math.random() * array.length))
+        const randomCard = array[rand]
+        return {randomCard , rand}
+    }
+
+    const getCard = () => {
+        const x = getRandom(cards)
+        const xCard = x.randomCard
+        const xIdx = x.rand
+        const y = getRandom(suits).randomCard
+        const newCard = {
+            id: xCard + y,
+            value : xIdx
+        }
+        return newCard
+    }
+    return { getCard }
+})();
+
+console.log(getRandomCard.getCard())
+
+const createCardArray = (amount) => {
+    let cardArray = []
+    for (let i = 0; i < amount; i++) {
+        const element = getRandomCard.getCard();
+        cardArray.push(element)
+    }
+    return cardArray
 }
 
-const getRandomCard  = (cards, suits) => {
-    cardsContainer.appendChild
-}
+const newArr = createCardArray(10)
+console.log(newArr)
 
-
-let displayedCards = []
 
 const render = (array) => {
 
